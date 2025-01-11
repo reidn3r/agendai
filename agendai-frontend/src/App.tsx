853a2200922a38173073a73cdc0da3393450b74d
Reidner <reidnersteam@hotmail.com>
2025-01-11 15:13:30 -0300

    shadcn lib working

--- agendai-frontend/src/App.tsx
+++ agendai-frontend/src/App.tsx
@@ -1,7 +1,19 @@
+import { Button } from "./components/ui/button";
+
 export default function App() {
+
+  const handleClick = () => {
+    alert("Teste");
+  }
+
   return (
-    <h1 className="text-3xl font-bold underline text-neutral-900">
-      Hello world!
-    </h1>
+    <div className="flex flex-col w-100 items-center">
+      <h1 className="text-3xl font-bold underline text-blue-900">
+        Hello world!
+      </h1>
+      <Button
+        onClick={handleClick}
+      >Teste</Button>
+    </div>
   )
 }
