23812b347f0224dceabd9f39d5122a4d51c89108
Vitor Azevedo Padovani <98499539+Vitor-Padovani@users.noreply.github.com>
2025-01-12 19:22:48 -0300

    navbar

--- agendai-frontend/src/pages/Default.tsx
+++ agendai-frontend/src/pages/Default.tsx
@@ -13,15 +13,42 @@ export default function Home(){
     const handleHome = () => {
         navigate("/home")    
     }
+
+    const handleAction = (action) => {
+        alert(`You clicked ${action}`);
+      };
+      
     return(
         <>
-            <p className="bg-red-300">Página Padrão</p>
-            <Button
-                onClick={handleLogin}
-            >Ir para Login</Button>
-            <Button
-                onClick={handleHome}
-            >Ir para Home</Button>
+            <nav className="bg-blue-600 text-white px-4 py-2 flex justify-between items-center shadow-md">
+      <h1 className="text-lg font-bold">Menu Rápido</h1>
+      <div className="space-x-4">
+        <button
+          onClick={() => handleAction("Create")}
+          className="btn btn-navbar"
+        >
+          Create
+        </button>
+        <button
+          onClick={() => handleAction("Read")}
+          className="btn btn-navbar"
+        >
+          Read
+        </button>
+        <button
+          onClick={() => handleAction("Update")}
+          className="btn btn-navbar"
+        >
+          Update
+        </button>
+        <button
+          onClick={() => handleAction("Delete")}
+          className="btn btn-navbar"
+        >
+          Delete
+        </button>
+      </div>
+    </nav>
         </>
     )
 }
